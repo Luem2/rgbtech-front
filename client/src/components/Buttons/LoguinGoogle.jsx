223aa31ad7d379928e74b5e37f4e48bf4c48205d
Luem <luchemma@gmail.com>
2022-08-31 20:54:45 -0300

    Update LoguinGoogle.jsx

--- client/src/components/Buttons/LoguinGoogle.jsx
+++ client/src/components/Buttons/LoguinGoogle.jsx
@@ -1,47 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
 function LoguinGoogle() {
+  // const [user, setUser] = useState({})
 
-    function handleCallbackResponse(response) {
-        console.log(" Encoded JWT ID token : " + response.credential);
-        var userObject = jwt_decode(response.credential);
-        console.log(userObject);
-        setUser(userObject);
-        document.getElementById("signInDiv").hidden = true;
-    }
+  function handleCallbackResponse(response) {
+    console.log(' Encoded JWT ID token : ' + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById('signInDiv').hidden = true;
+  }
 
-    function handleSignOut(event)
-    setUser({});
-    document.getElementById("signInDiv").hidden = false;
-    3
+  // function handleSignOut(event)
+  // setUser({});
+  // document.getElementById("signInDiv").hidden = false;
 
-    useEffect(() => {
-        /*global google*/
-        google.accounts.id.initialize({
-            client_id: "319669614492-i7e6o766ctapimibesbnj4g2c9fkvk80.apps.googleusercontent.com",
-            callback: handleCallbackResponse
-        })
-        google.accounts.id.renderButton(
-            document.getElementById("signInDiv"),
-            { theme: "outline", size: "large" }
+  useEffect(() => {
+    /*global google*/
+    google.accounts.id.initialize({
+      client_id:
+        '319669614492-i7e6o766ctapimibesbnj4g2c9fkvk80.apps.googleusercontent.com',
+      callback: handleCallbackResponse,
+    });
+    google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+      theme: 'outline',
+      size: 'large',
+    });
+    // google.accounts.id.prompt ( ) ;
+  }, []);
 
-        );
-        google.accounts.id.prompt ( ) ;
-
-    }, []);
-
-    return (
+  return (
+    <div>
+      <div div id=' signInDiv '>
+        {' '}
+      </div>
+      {Object.keys(user).length != 0 && (
+        <button onClick={e => handleSignOut(e)}> Sign Out </button>
+      )}
+      {user && (
         <div>
-            <div div id=" signInDiv " > </div>
-            {Object.keys(user).length != 0 &&
-            < button onClick={(e) => handleSignOut(e)} > Sign Out </button >}
-            {user &&
-                <div>
-                    <img src={user.picture} />
-                    <h3>{user.name}</h3>
-                </div>}
+          <img src={user.picture} />
+          <h3>{user.name}</h3>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 export default LoguinGoogle;
